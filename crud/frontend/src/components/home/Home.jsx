5826e991fcdeeee256ff5a82dca8096a73d3b329
Santiagososs <sheriffhermoso@gmail.com>
2020-09-29 21:21:49 -0300

    Commit Crud React

---
+++ crud/frontend/src/components/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+    <Main icon= "home" title = "Início"
+         subtitle = "Hydra Corporações.">
+         <div className="display-4">Bem Vindo!</div>   
+         <hr /> 
+         <p className="mb-0">Cadastre-se no nosso sistema!</p>
+    </Main>
+
+
+
---
+++ crud/frontend/src/components/template/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+import React from 'react'
+
+export default props => 
+    <footer className="footer">
+       <span>
+           Desenvolvido por 
+           <strong> Sachetty <span className="text-danger">e</span> Santiago</strong>
+       </span>
+    </footer>
